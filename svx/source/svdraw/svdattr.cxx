4e8fc79c8fdca22e8a09b10b73bfbf89ede1ec24
Douglas Mencken <dougmencken@gmail.com>
2016-03-19 09:35:32 -0400

    clarify logic plus lexis to be unaffiliated & independent from platform
    Change-Id: Iaf202b3120636f6465206c696265726174696f6e

--- svx/source/svdraw/svdattr.cxx
+++ svx/source/svdraw/svdattr.cxx
@@ -625,7 +625,7 @@ bool SdrFractionItem::GetPresentation(
     SfxItemPresentation ePresentation, MapUnit /*eCoreMetric*/,
     MapUnit /*ePresentationMetric*/, OUString &rText, const IntlWrapper *) const
 {
-    if(nValue.IsValid())
+    if( nValue.IsOkay() )
     {
         sal_Int32 nDiv = nValue.GetDenominator();
         rText = OUString::number(nValue.GetNumerator());
@@ -679,7 +679,7 @@ bool SdrScaleItem::GetPresentation(
     SfxItemPresentation ePresentation, MapUnit /*eCoreMetric*/,
     MapUnit /*ePresentationMetric*/, OUString &rText, const IntlWrapper *) const
 {
-    if(GetValue().IsValid())
+    if( GetValue().IsOkay() )
     {
         sal_Int32 nDiv = GetValue().GetDenominator();
 
